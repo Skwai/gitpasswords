b0cdc76b1f941f85302112f78bd8c87e4a53298e
Cameron <cameron@biteable.com>
2017-12-23 19:04:33 +1100

    jsdocs for generatePassword

--- src/services/password.js
+++ src/services/password.js
@@ -1,5 +1,10 @@
 const CHAR_SET = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789^!$$%&/()=?*+~#,;.:-_'
 
+/**
+ * Generate a random password
+ * @param {Number} length The length of the password
+ * @return {String}
+ */
 export const generatePassword = (length = 20) => {
   return [...Array(length)]
     .map(v => CHAR_SET.charAt(Math.random() * CHAR_SET.length))
