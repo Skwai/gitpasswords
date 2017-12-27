8155b5914c6ef7c9f4291d21167f03650623381c
Cameron <cameron@biteable.com>
2017-12-28 10:44:46 +1100

    tidier storage reference

--- src/store/initial-state.js
+++ src/store/initial-state.js
@@ -1,8 +1,10 @@
 import { ACCESS_TOKEN_STORAGE_KEY } from '@/config'
 
+const storage = window.localStorage || false
+
 export default {
   username: null,
-  token: window.localStorage ? window.localStorage.getItem(ACCESS_TOKEN_STORAGE_KEY) : null,
+  token: storage ? storage.getItem(ACCESS_TOKEN_STORAGE_KEY) : null,
   secret: null,
   filename: null,
   gistID: null,
